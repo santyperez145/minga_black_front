192f202013c8ae868fa7979c451b2eda4654f316
santyperez145 <santyperez145s@gmail.com>
2023-06-28 00:00:13 -0300

    minga-11

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,8 @@
-export default function Search({ search }) {
+export default function Search({ title }) {
   return (
-    <div className='flex flex-wrap py-[10px] items-center gap-[60px] self-stretch rounded-md border-[#42869d] border-2 bg-white xl:bg-[#9D9D9D4D] opacity-40 shadow-white'>
-        <img className='flex bg-black xl:bg-[#9D9D9D4D] flex-wrap ml-[17px] ' src="/search.png" alt="s" />
-        <p className='text-center flex-wrap text-black xl:text-white opacity-100 text-xl'>{search}</p>
+    <div className='flex py-[10px] items-center gap-[60px] self-stretch rounded-md border-[#42869d] border-2 bg-white xl:bg-[#9D9D9D4D] opacity-40 shadow-white'>
+        <img className='flex bg-black xl:bg-[#9D9D9D4D] ml-[17px] ' src="/search.png" alt="s" />
+        <p className='text-center text-black xl:text-white opacity-100 text-xl'>{title}</p>
     </div>
   )
 }
