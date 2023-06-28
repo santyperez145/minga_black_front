5a274d4348e18650e3ca8b9753560cc0a84ddec9
santyperez145 <133786855+santyperez145@users.noreply.github.com>
2023-06-28 01:21:30 -0300

    Update Index.jsx

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -6,7 +6,7 @@ export default function Index({ nombre,version,data }) {
     console.log(version)
     console.log(data)
   return (
-    <main className='flex flex-wrap justify-center w-auto min-h-[95vh] text-white bg-[url("../public/fondo2.png")] xl:bg-[url(hidden)] bg-contain bg-center bg-no-repeat xl:inline-flex xl:justify-evenly items-start gap-[24px] xl:w-[100%]'>
+    <main className='flex flex-wrap justify-center w-auto min-h-[95vh] text-white bg-[url("../public/fondo1.png")] xl:bg-[url(hidden)] bg-contain bg-center bg-no-repeat xl:inline-flex xl:justify-evenly items-start gap-[24px] xl:w-[100%]'>
         <h1 className='hidden flex-wrap xl:w-[100%] xl:justify-center xl:flex xl:self-center xl:text-6xl xl:text-center'>Best manga reader</h1>
         <Carrousel />
         <Hero />  
