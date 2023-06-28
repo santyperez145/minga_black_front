7f7d7a118c9045ba2d63b1ef1d5b24facdca400a
santyperez145 <133786855+santyperez145@users.noreply.github.com>
2023-06-27 21:16:49 -0300

    minga-10 (#2)

---
+++ src/components/Carrousel.jsx
@@ -0,0 +1,7 @@
+export default function Carrousel() {
+  return (
+    <div className='flex flex-wrap flex-row self-center justify-evenly w-[50%] xl:flex-wrap'>
+        <img className='hidden flex-wrap xl:flex xl:items-center xl:w-[640px] xl:h-[850px]' src="fondo2.png" alt="" />
+    </div> 
+  )
+}
---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import Search from "./Search"
+
+export default function Hero() {
+  return (
+    <div className='flex flex-wrap h-[276] flex-col font-sans justify-center items-center gap-[25px] mx-[21px] xl:flex-wrap xl:gap-[24px] xl:self-center xl:w-[50] xl:m-[0px] xl:h-[326px]'>
+        <h2 className='flex text-center flex-wrap items-center font-sans flex-col font-bold text-4xl xl:text-left xl:self-start xl:w-[360px]'>Your favourite manga reader 😏</h2>
+        <p className='flex flex-wrap text-center text-[16px] md:text-[25px] xl:text-[16px] xl:text-justify xl:w-[391px] xl:self-start' >is an exceptional app for all manga lovers. With a wide range of titles available, from classics to the latest releases, this app is perfect for those who want to read manga anytime, anywhere.</p>
+        <Search search='Search mangas' />
+    </div>  
+  )
+}
