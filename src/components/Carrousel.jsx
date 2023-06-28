7c88ad841131da0e160f206deb41519b31b57018
santyperez145 <133786855+santyperez145@users.noreply.github.com>
2023-06-28 01:12:29 -0300

    minga-12 (#5)

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -1,7 +1,33 @@
+import { useState, useEffect } from 'react';
+
 export default function Carrousel() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [images, setImages] = useState([
+    'fondo1.png',
+    'fondo2.png',
+    'fondo3.png',
+    // Agrega aquí más nombres de imágenes si es necesario
+  ]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) =>
+        prevIndex === images.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 4000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [images.length]);
+
   return (
     <div className='flex flex-wrap flex-row self-center justify-evenly w-[50%] xl:flex-wrap'>
-        <img className='hidden flex-wrap xl:flex xl:items-center xl:w-[640px] xl:h-[850px]' src="fondo2.png" alt="" />
-    </div> 
-  )
+      <img
+        className='hidden flex-wrap xl:flex xl:items-center xl:w-[700px] xl:h-[850px]'
+        src={images[currentIndex]}
+        alt={`Imagen ${currentIndex + 1}`}
+      />
+    </div>
+  );
 }
